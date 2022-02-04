8384e781a43f77c07a2494c8be145b5dec1cd069
amarshikhar <amar.shikhar007@gmail.com>
2022-02-04 19:09:28 +0530

    make it better

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
-app = Flask(__name__)
+app = Flask(__name__)#flask app created
 
 @app.route('/',methods=['GET'])  # route to display the home page
-@cross_origin()
+@cross_origin()#not required for local deployment
 def homePage():
     return render_template("index.html")
 
@@ -17,20 +17,20 @@ def index():
     if request.method == 'POST':
         try:
             searchString = request.form['content'].replace(" ","")
-            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
+            flipkart_url = "https://www.flipkart.com/search?q=" + searchString#to find phone n
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
             uClient.close()
-            flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
+            flipkart_html = bs(flipkartPage, "html.parser")#beautiful soup
+            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})#finding from html data
+            del bigboxes[0:3]#len(bigboxes)=27,27 mobile info available
             box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']#to find href to go inside page
+            prodRes = requests.get(productLink)#to go to page
             prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
+            prod_html = bs(prodRes.text, "html.parser")#beautiful soup
             print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})#find reviews in page
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
@@ -40,7 +40,7 @@ def index():
             for commentbox in commentboxes:
                 try:
                     #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text#find name
 
                 except:
                     name = 'No Name'
